7a25345a18da3dabcca12b5091a2befbe9f3fa05
KayyLanne <kaylanne.regina@Hotmail.com>
2023-04-25 04:58:18 -0300

    add paginações

--- bar/urls.py
+++ bar/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from produtos.views import home
+from produtos.views import home, login, cadastro, produto
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home),
+    path('login/', login),
+    path('cadastro/', cadastro),
+    path('produto/', produto),
 ]
--- produtos/views.py
+++ produtos/views.py
@@ -2,3 +2,12 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html')
+
+def login(request):
+    return render(request, 'login.html')
+
+def cadastro(request):
+    return render(request, 'cadastro.html')
+
+def produto(request):
+    return render(request, 'produto.html')
